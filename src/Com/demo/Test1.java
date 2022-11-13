8e4d497a32a40d45a2dbecf79cc849dc44abf26a
aryak <aryak@LAPTOP-D419D3VT>
2022-11-13 16:15:27 +0530

    first Commit

---
+++ src/Com/demo/Test1.java
@@ -0,0 +1,21 @@
+package Com.demo;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String hunary="yes";
+		
+		
+		if(hunary=="yes")
+		{
+			System.out.println("Eat somethings...");
+		}
+		else
+		{
+			System.out.println("Do work...");
+		}
+	}
+
+}
