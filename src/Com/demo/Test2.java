17a3544d4103b11abc29ddb5a019d77121a6ed01
akshay <akshaychaudhari12995@gmail.com>
2022-11-14 17:57:55 +0530

    First commited changes in yes by akshay

---
+++ src/Com/demo/Test2.java
@@ -0,0 +1,21 @@
+package Com.demo;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String hunary="y";
+		
+		
+		if(hunary=="yes")
+		{
+			System.out.println("Eat somethings...");
+		}
+		else
+		{
+			System.out.println("Do work...");
+		}
+	}
+
+}
